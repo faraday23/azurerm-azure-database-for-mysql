49e1e22cefda679cb1ae59e6eed56543dbac6fe6
faraday23 <41235618+faraday23@users.noreply.github.com>
2020-09-17 09:01:39 -0400

    Create output.tf

---
+++ module update/output.tf
@@ -0,0 +1,27 @@
+output "administrator_login" {
+  value       = var.administrator_login
+  sensitive   = true
+  description = "The MySQL instance login for the admin."
+}
+
+output "password" {
+  value       = random_password.login_password.result
+  sensitive   = true
+  description = "The MySQL instance password for the admin."
+}
+
+output "server_name" {
+  value       = azurerm_mysql_server.primary.name
+  description = "The Name of the mysql instance."
+}
+
+output "server_id" {
+  value       = azurerm_mysql_server.primary.id
+  description = "The ID of the mysql instance."
+}
+
+output "primary_fqdn" {
+  value       = azurerm_mysql_server.primary.fqdn
+  description = "The FQDN of the mysql instance."
+}
+
