c84d9de7e1bc18774d72ec47291109957390109a
faraday23 <41235618+faraday23@users.noreply.github.com>
2020-10-05 10:59:03 -0400

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,7 +1,40 @@
-# Configure Providers
+# required server inputs
+variable "srvr_id" {
+  description = "identifier appended to srvr name for more info see https://github.com/[redacted]/python-azure-naming"
+  type        = string
+}
+
+variable "srvr_id_replica" {
+  description = "identifier appended to srvr name for more info see https://github.com/[redacted]/python-azure-naming"
+  type        = string
+}
+
+# required tags
+variable "names" {
+  description = "names to be applied to resources"
+  type        = map(string)
+}
+
+variable "tags" {
+  description = "tags to be applied to resources"
+  type        = map(string)
+}
+
+#diagnostic setting required variables
+variable "sa_resource_group" {
+    type        = string
+    description = "Name of resource group the storage account resides in"
+}
+
+variable "storage_account" {
+    type        = string
+    description = "Name of storage account"
+}
+
+# Configure Azure Providers
 provider "azurerm" {
-  version = ">=2.0.0"
-  subscription_id = "b0837458-adf3-41b0-a8fb-c16f9719627d"
+  version = ">=2.25.0"
+  subscription_id = "00000000-0000-0000-0000-00000000"
   features {}
 }
 
@@ -11,156 +44,93 @@ provider "azurerm" {
 
 module "subscription" {
   source          = "github.com/Azure-Terraform/terraform-azurerm-subscription-data.git?ref=v1.0.0"
-  subscription_id = "b0837458-adf3-41b0-a8fb-c16f9719627d"
+  subscription_id = "00000000-0000-0000-0000-00000000"
 }
 
 module "rules" {
-  source = "git@github.com:openrba/python-azure-naming.git?ref=tf"
+  source = "git@github.com:[redacted]/python-azure-naming.git?ref=tf"
 }
 
 # For tags and info see https://github.com/Azure-Terraform/terraform-azurerm-metadata 
-# For naming convention see https://github.com/openrba/python-azure-naming 
+# For naming convention see https://github.com/[redacted]/python-azure-naming 
 module "metadata" {
-  source = "github.com/Azure-Terraform/terraform-azurerm-metadata.git?ref=v1.0.0"
+  source = "github.com/Azure-Terraform/terraform-azurerm-metadata.git?ref=v1.1.0"
 
   naming_rules = module.rules.yaml
   
   market              = "us"
-  location            = "useast1"
+  location            = "uscent1" # for location list see - https://github.com/[redacted]/python-azure-naming#rbaazureregion
   sre_team            = "alpha"
-  environment         = "sandbox"
-  project             = "mysqlDB"
-  business_unit       = "iog"
+  environment         = "sandbox" # for environment list see - https://github.com/[redacted]/python-azure-naming#rbaenvironment
+  project             = "mysql"
+  business_unit       = "iog"     
   product_group       = "tfe"
-  product_name        = "mysqlsrvr"
+  product_name        = "mysql"   # for product name list see - https://github.com/[redacted]/python-azure-naming#rbaproductname
   subscription_id     = module.subscription.output.subscription_id
   subscription_type   = "nonprod"
   resource_group_type = "app"
-}
-
-module "resource_group" {
-  source = "github.com/Azure-Terraform/terraform-azurerm-resource-group.git?ref=v1.0.0"
-  
-  location = module.metadata.location
-  names    = module.metadata.names
-  tags     = module.metadata.tags
 }
 
 # mysql-server storage account
 module "storage_acct" {
   source = "../mysql-module-test/storage_account"
   # Required inputs 
-  db_id                 = "1337"
-  
+  srvr_id               = "01"
   # Pre-Built Modules  
   location              = module.metadata.location
   names                 = module.metadata.names
   tags                  = module.metadata.tags
-  resource_group_name   = module.resource_group.name
-}
-
-# deploy new vnet for private link endpoint+
-module "virtual_network" {
-  source = "github.com/Azure-Terraform/terraform-azurerm-virtual-network.git?ref=v1.0.0"
-
-  naming_rules = module.rules.yaml
-
-  # Pre-Built Modules
-  resource_group_name = module.resource_group.name
-  location            = module.resource_group.location
-  names               = module.metadata.names
-  tags                = module.metadata.tags
-
-  address_space = ["192.168.123.0/24"]
-
-  subnets = {
-    "01-iaas-private"     = ["192.168.123.0/27"]
-    "02-iaas-public"      = ["192.168.123.32/27"]
-    "03-iaas-outbound"    = ["192.168.123.64/27"]
-  }
-}
-
-# Manages a subnet. Subnets represent network segments within the IP space defined by the virtual network.
-module "snet_endpoint" {
-  source = "../mysql-module-test/snet_endpoint"
-  # Required inputs 
-  virtual_network_name = module.virtual_network.vnet
-  subnet_cidr          = module.virtual_network.subnet["iaas-private-subnet"].address_prefixes
-  enforce_private_link_endpoint_network_policies = true
-  db_id                = "1337"
-
-  # Pre-Built Modules  
-  names               = module.metadata.names
-  tags                = module.metadata.tags
-  resource_group_name = module.resource_group.name
+  resource_group_name   = "rg-azure-demo-mysql"
 }
 
 # mysql-server module
 module "mysql_server" {
-  source = "../mysql-module-test/mysql_server"
-  # Required inputs 
-  db_id                       = "1337"
-  create_mode                 = "Default"
-
-  threat_detection_policy = [{
-    enabled                     = true   
-    storage_endpoint            = module.storage_acct.primary_blob_endpoint
-    storage_account_access_key  = module.storage_acct.primary_access_key         
-    retention_days              = 7
-  }]
-
+  source = "../mysql-module-test/mysql_server_vnet_rule"
   # Pre-Built Modules  
-  location            = module.metadata.location
-  names               = module.metadata.names
-  tags                = module.metadata.tags
-  resource_group_name = module.resource_group.name
-}
-
-module "private_dns_zone" {
-  source = "../mysql-module-test/private_dns_zone"
+  location                  = module.metadata.location
+  names                     = module.metadata.names
+  tags                      = module.metadata.tags
   # Required inputs 
-  private_dns_zone_name = "privatelink.mysql.database.azure.com"
-
-  # Pre-Built Modules  
-  resource_group_name   = module.resource_group.name
+  srvr_id                   = "01"
+  srvr_id_replica           = "03"
+  resource_group_name       = "rg-azure-demo-mysql"
+  event_scheduler           = "ON"
+  # Enable creation of Database 
+  enable_primary            = false
+  enable_db                 = false
+  # Replica server required inputs
+  enable_replica            = true
+  create_mode               = "Replica"
+  creation_source_server_id = "/subscriptions/00000000-0000-0000-0000-00000000/resourceGroups/rg-azure-demo-mysql/providers/Microsoft.DBforMySQL/servers/mysql-useast1-sandbox01/"
+  replica_server_location   = "centralus"  #must be in different location than primary server, use this azure cli command to find available locations 'az account list-locations -o table'
+  # MySQL server and database audit policies and advanced threat protection 
+  enable_threat_detection_policy = false
+  # Storage endpoints for atp logs
+  storage_endpoint               = module.storage_acct.primary_blob_endpoint
+  storage_account_access_key     = module.storage_acct.primary_access_key  
+  # Storage account for disagnostics logs
+  sa_resource_group = "rg-azure-demo-mysql"
+  storage_account   = "sqlvalhqvn4pesccwq"
+  # Enable azure ad admin
+  enable_mysql_ad_admin          = false
+  ad_admin_login_name            = "bagbru01@risk.regn.net"
+  ad_admin_login_name_replica    = "emerwe01@risk.regn.net"
+  # private link endpoint
+  enable_private_endpoint        = false 
+  public_network_access_enabled  = true      # public access will need to be enabled to use vnet rules
+   # Vnet rule
+  enable_vnet_rule               = false
+  # Virtual network - for Existing virtual network
+  vnet_resource_group_name         = "rg-azure-demo-mysql"             #must be existing resource group within same region as primary server
+  vnet_replica_resource_group_name = "rg-azure-demo-mysql-centralus"   #must be existing resource group within same region as replica server
+  virtual_network_name             = "vnet-sandbox-eastus-mysql-01"    #must be existing vnet with available address space
+  virtual_network_name_replica     = "vnet-sandbox-centralus-mysql-03" #must be existing vnet with available address space
+  subnet_name_primary              = "default" #must be existing subnet name 
+  subnet_name_replica              = "default" #must be existing subnet name 
+  # Firewall Rules to allow client IP
+  enable_firewall_rules           = false
+  firewall_rules = [
+                {name             = "desktop-ip"
+                start_ip_address  = "209.243.55.98"
+                end_ip_address    = "209.243.55.98"}]
 }
-
-module "private_dns_zone_virtual_network_link" {
-  source = "../mysql-module-test/private_dns_zone_virtual_network_link"
-  # Required inputs 
-  private_dns_zone_name = module.private_dns_zone.private_dns_zone_name
-  virtual_network_id    = module.virtual_network.vnet.id
-  db_id                 = "1337"
-
-  # Pre-Built Modules  
-  resource_group_name   = module.resource_group.name
-}
-
-module "private_link_endpoint" {
-  source = "../mysql-module-test/private_link_endpoint"
-  # Required inputs 
-  subnet_id             = module.snet_endpoint.id
-  db_id                 = "1337"
-   # Pre-Built Modules  
-  location              = module.metadata.location
-  names                 = module.metadata.names
-  tags                  = module.metadata.tags
-  resource_group_name   = module.resource_group.name
-
-  private_service_connection = [{
-    name                           = "prv-serv-conn-${var.names.product_name}-${var.names.environment}-mysql${var.db_id}"
-    private_connection_resource_id = module.mysql_server.id
-    subresource_names              = ["mysqlServer"]
-    is_manual_connection           = false
-  }]
-
-  private_dns_zone_group = [{
-    name                 = module.private_dns_zone.private_dns_zone_name
-    private_dns_zone_ids = module.private_dns_zone.id
-  }]
-}
-
-
-
-
-
